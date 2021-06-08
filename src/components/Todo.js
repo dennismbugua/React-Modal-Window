b874b15ec21777e00f29448c607f289ee6724ca6
Mc-D Lonell <mc-dlonell@live.com>
2021-06-08 21:23:04 +0300

    Modal Window

---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+  return (
+    <div className="card">
+      <h2>{props.title}</h2>
+      <div>
+        <button className="actions" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+    </div>
+  );
+}
+export default Todo;
